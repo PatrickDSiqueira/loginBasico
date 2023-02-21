eeea28814ace214bbafddf993817a0116d2c5570
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-21 17:23:47 -0300

    primeiro commit

---
+++ resources/views/Produto.blade.php
@@ -0,0 +1,11 @@
+<h3>Lista de Produtos</h3>
+
+<ul>
+    <li>Marcarrao</li>
+    <li>Melancia</li>
+    <li>Melao</li>
+    <li>Leite</li>
+    <li>Pão</li>
+    <li>Maça</li>
+    <li>Manga</li>
+</ul>
